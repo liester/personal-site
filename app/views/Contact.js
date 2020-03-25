18ab19db79edfa57cebe67492312882df80fb553
Levi Liester <levi.liester@gmail.com>
2020-03-24 23:49:36 -0500

    Didn't work, trying some modifications

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -94,9 +94,9 @@ const Contact = () => {
             onMouseEnter={() => setIsActive(false)}
             onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
           >
-            <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
-              <span>{message}</span>
-              <span>@mldangelo.com</span>
+            <a href={validateText(message) ? 'mailto:levi.liester@gmail.com' : ''}>
+              <span>levi.liester</span>
+              <span>@gmail.com</span>
             </a>
           </div>
         </div>
